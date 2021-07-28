09b04ea6d3760edc520f4481ca05f876d89414ec
mikeboyddev <mikeboyd9@gmail.com>
2021-07-28 18:52:20 -0400

    fixes 1

--- scripts/script.js
+++ scripts/script.js
@@ -1,24 +1,26 @@
-const modalEl = document.querySelector('.modal');
-const modalEditBtn = document.querySelector('.profile__edit-btn');
-const modalCloseBtn = document.querySelector('.modal__button');
-const modalFormEl = document.querySelector('#modal-edit-form');
-const modalNameInput = document.querySelector('#modal-name-input');
-const modalOccupationInput = document.querySelector('#modal-occupation-input');
-const cardName = document.querySelector('.js-profile-name');
-const cardOccupation = document.querySelector('.js-profile-occupation');
+const modalEl = document.querySelector('.modal')
+const modalEditBtn = document.querySelector('.profile__edit-btn')
+const modalCloseBtn = document.querySelector('.modal__button')
+const modalFormEl = document.querySelector('#modal-edit-form')
+const modalNameInput = document.querySelector('#modal-name-input')
+const modalOccupationInput = document.querySelector('#modal-occupation-input')
+const cardName = document.querySelector('.profile__name')
+const cardOccupation = document.querySelector('.profile__occupation')
 
+function enableModal() {
+  modalEl.classList.add('modal_enabled')
+}
+function disableModal() {
+  modalEl.classList.remove('modal_enabled')
+}
 
-modalEditBtn.addEventListener('click', function() {
-    modalEl.classList.add('modal_enabled');
-})
+modalEditBtn.addEventListener('click', enableModal)
 
-modalCloseBtn.addEventListener('click', function() {
-    modalEl.classList.remove('modal_enabled');
-})
+modalCloseBtn.addEventListener('click', disableModal)
 
-modalFormEl.addEventListener('submit', function(e) {
-    e.preventDefault();
-    cardName.textContent = modalNameInput.value;
-    cardOccupation.textContent = modalOccupationInput.value;
-    modalEl.classList.remove('modal_enabled');
+modalFormEl.addEventListener('submit', function (e) {
+  e.preventDefault()
+  cardName.textContent = modalNameInput.value
+  cardOccupation.textContent = modalOccupationInput.value
+  disableModal()
 })
